0bfbbf37eb5b74fad0f78371a95460093f5040dd
nateazevedo <nathanielazevedo@hotmail.com>
2023-05-21 00:04:14 -0700

    clean up

--- controllers/Party.js
+++ controllers/Party.js
@@ -4,7 +4,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 const sdk = yes('@yelp-developers/v1.0#deudoolf6o9f51');
 const yelpKey = process.env.YELP;
+
 // Get Party
+// return party
 export const getParty = async (req, res) => {
   const id = req.params.id;
   try {
@@ -19,57 +21,42 @@ export const getParty = async (req, res) => {
   }
 };
 
-const rLikesToObj = (rLikes, party) => {
+const likesToObj = (rLikes, obj) => {
   rLikes.forEach((vote) => {
-    if (!party.votes.hasOwnProperty(vote)) {
-      party.votes[vote] = 0;
-    } else {
-      party.votes[vote]++;
-    }
+    if (!obj.hasOwnProperty(vote)) obj[vote] = 0;
+    else obj[vote]++;
   });
-  return party.votes;
+  return obj;
 };
 
-const getRWinner = (party) => {
-  const votes = party.votes;
+const getWinner = (obj, votes) => {
   const max = Math.max(...Object.values(votes));
   const winner = Object.keys(votes).find((key) => votes[key] === max);
-  const result = party.restaurants.find((r) => r.id === winner);
+  const result = obj.find((r) => r.id === winner);
   return result;
 };
 
-const getTWinner = (party) => {
-  const votes = party.hours;
-  const max = Math.max(...Object.values(votes));
-  const winner = Object.keys(votes).find((key) => votes[key] === max);
-  return winner;
-};
-
 // Vote Party
+// return updated party
 export const voteParty = async (req, res) => {
   const id = req.params.id;
   const { rLikes, tLikes } = req.body;
-
   try {
     const party = await Party.findOne({ _id: id });
-    const newVotes = rLikesToObj(rLikes, party);
-    const newVoters = Number(party.voters) + 1;
-
-    if (newVoters === party.maxVoters) {
-      // if all voters have voted
-      const winner = getRWinner(party);
-      const tWinner = getTWinner(party);
-      tLikes.winner = tWinner;
+    const r_votes = likesToObj(rLikes, party.r_votes);
+    const t_votes = tLikes ? likesToObj(tLikes, party.t_votes) : null;
+    const voters_so_far = Number(party.voters_so_far) + 1;
+    if (voters_so_far === party.max_voters) {
+      const r_winner = getWinner(party.restaurants, party.r_votes);
+      const t_winner = tLikes
+        ? getWinner(party.times_to_vote_on, party.t_votes)
+        : null;
       await Party.updateOne(
         { _id: id },
-        { winner, voters: newVoters, votes: newVotes, hours: tLikes }
+        { r_winner, t_winner, voters_so_far, r_votes, t_votes }
       );
     } else {
-      // if not all voters have voted
-      await Party.updateOne(
-        { _id: id },
-        { votes: newVotes, voters: newVoters, hours: tLikes }
-      );
+      await Party.updateOne({ _id: id }, { voters_so_far, r_votes, t_votes });
     }
     const updatedParty = await Party.findOne({ _id: id });
     res.status(201).json(updatedParty);
@@ -80,12 +67,12 @@ export const voteParty = async (req, res) => {
 };
 
 // Get Restaurants
+// return array of restaurants
 const getRestaurants = async (
   location,
   max_distance,
   number_of_restaurants
 ) => {
-  console.log(max_distance);
   try {
     sdk.auth(`Bearer ${yelpKey}`);
     return sdk
@@ -97,7 +84,6 @@ const getRestaurants = async (
         limit: number_of_restaurants,
       })
       .then(({ data }) => {
-        console.log(data);
         return data.businesses;
       })
       .catch((err) => {
@@ -110,6 +96,7 @@ const getRestaurants = async (
 };
 
 // Fetch Restaurants
+// return array of restaurants
 export const fetchRestaurants = async (req, res) => {
   const { location, max_distance, number_of_restaurants } = req.body;
   try {
@@ -132,51 +119,32 @@ export const fetchRestaurants = async (req, res) => {
 };
 
 // Create Party
+// return id and name
 export const createParty = async (req, res) => {
   try {
-    const {
-      name,
-      location,
-      max_distance,
-      maxVoters,
-      password,
-      number_of_restaurants,
+    const { restaurants, times_to_vote_on } = req.body;
+    const r_votes = {};
+    const t_votes = {};
+    restaurants.forEach((restaurant) => (r_votes[restaurant.id] = 0));
+    times_to_vote_on.forEach((time) => (t_votes[time.id] = 0));
+    const party = new Party({
+      ...req.body,
       restaurants,
-      voteTime,
-      hours,
-    } = req.body;
-    console.log(hours);
-    // const restaurants = await getRestaurants(location, maxDistance);
-    if (restaurants?.data?.error) {
-      res
-        .status(404)
-        .json({ message: restaurants.data.error.description })
-        .send();
-      return;
-    }
-    const votes = {};
-    restaurants.forEach((restaurant) => (votes[restaurant.id] = 0));
-    const newParty = new Party({
-      name,
-      restaurants,
-      maxVoters,
-      password,
-      voters: 0,
-      votes,
-      voteTime,
-      hours,
-      location,
-      max_distance,
-      number_of_restaurants,
+      r_winner: null,
+      t_winner: null,
+      voters_so_far: 0,
+      r_votes,
+      t_votes,
     });
-    const newerParty = await newParty.save();
-    res.status(200).json(newerParty);
+    const newParty = await party.save();
+    res.status(200).json(newParty);
   } catch (err) {
     res.status(409).json({ message: err.message });
   }
 };
 
 // Validate Password
+// return boolean
 export const validatePassword = async (req, res) => {
   const id = req.params.id;
   try {
@@ -192,22 +160,20 @@ export const validatePassword = async (req, res) => {
 };
 
 // Finish Party
+// return new party
 export const endParty = async (req, res) => {
   const id = req.params.id;
   try {
     const party = await Party.findOne({ _id: id });
-    const votes = party.votes;
-    const max = Math.max(...Object.values(votes));
-    const winner = Object.keys(votes).find((key) => votes[key] === max);
-    const result = party.restaurants.find((r) => r.id === winner);
-    await Party.updateOne({ _id: id }, { winner: result });
-    res.status(200).json({ winner: winner });
+    const r_winner = getWinner(party.restaurants, party.r_votes);
+    const t_winner = party.vote_on_time
+      ? getWinner(party.times_to_vote_on, party.t_votes).id
+      : null;
+    await Party.updateOne({ _id: id }, { r_winner, t_winner });
+    const updatedParty = await Party.findOne({ _id: id });
+    res.status(200).json(updatedParty);
   } catch (err) {
+    console.log(err);
     res.status(404).json({ message: err.message });
   }
 };
-
-/* 
-  If there is a top restaurant, it wins
-  If there is a tie, the computer randomly chooses a winner
-*/
--- models/Party.js
+++ models/Party.js
@@ -3,21 +3,23 @@ import mongoose from 'mongoose';
 const partySchema = new mongoose.Schema(
   {
     name: String,
-    restaurants: Array,
-    votes: Object,
-    password: String,
-    winner: Object,
-    maxVoters: Number,
-    voters: Number,
-    voteTime: Boolean,
-    hours: Object,
     location: String,
     max_distance: Number,
     number_of_restaurants: Number,
+    max_voters: Number,
+    times_to_vote_on: Array,
+    vote_on_time: Boolean,
+    password: String,
+    restaurants: Array,
+    r_votes: Object,
+    t_votes: Object,
+    r_winner: Object || null,
+    t_winner: String || null,
+    voters_so_far: Number,
   },
   { timestamps: true }
 );
 
-const Party = mongoose.model('partys', partySchema);
+const Party = mongoose.model('parties', partySchema);
 
 export default Party;
